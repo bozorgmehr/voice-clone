cb21afb350e061fbdc2b1cb8a25f1d81319c1e37
Joana Sousa <joanacoutsousa@gmail.com>
2023-12-09 16:41:03 +0000

    Add files via upload

---
+++ genai.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+
+# Load your API key from an environment variable or secret management service
+load_dotenv()  # take environment variables from .env
+
+client = OpenAI()
+
+def speech2text(audio_file):
+    transcript = client.audio.transcriptions.create(
+        model="whisper-1",
+        file=audio_file,
+        response_format="text"
+    )
+    return transcript
+
+def translate(translation, final_text):
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system",
+             "content": "You are a translater"},
+            {"role": "user",
+             "content": "Translate the text in" + translation + final_text}
+        ]
+    )
+    return completion.choices[0].message.content
